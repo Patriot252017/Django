3f6cccd8bdb183143cfe52e07b7c604c79b6922d
Ryan gosuslug <zahlaminn@yandex.ru>
2025-01-28 00:15:30 +1000

    2 endpoints in 1 + redirect

--- fefu/fefuapp/urls.py
+++ fefu/fefuapp/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-from . import views
-from .views import text_Post
+from .views import post_list_and_create
 
 urlpatterns = [
-    path('', views.home_view, name='home'),
-    path('about/', views.about_view, name='about'),
-    path('post/', text_Post, name='text_post'),
+    path('', post_list_and_create, name='home'),
 ]
--- fefu/fefuapp/views.py
+++ fefu/fefuapp/views.py
@@ -1,22 +1,13 @@
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from .models import PostData
-from django.views.decorators.csrf import csrf_exempt
 
-def home_view(request):
-    return HttpResponse("Hello, World!")
 
-def about_view(request):
-    return HttpResponse("О нас")
-
-@csrf_exempt
-def text_Post(request):
+def post_list_and_create(request):
     if request.method == "POST":
         text = request.POST.get("text", "")
         if text:
             PostData.objects.create(text=text)
-            return HttpResponse(f"<h1>Результат</h1><p>{text}</p>")
-        return HttpResponse("<h1>Ошибка</h1><p>Поле 'text' не может быть пустым</p>")
-    return HttpResponse("<h1>Ошибка</h1><p>Метод не поддерживается</p>")
+            return redirect(request.path)
 
-
-
+    posts = PostData.objects.all().order_by("-id")
+    return render(request, "home.html", {"posts": posts})
