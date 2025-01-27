2927437c9f5a2ad4588b85cdc716451aca09e6c2
Ryan gosuslug <zahlaminn@yandex.ru>
2025-01-27 19:21:09 +1000

    2 GET + 1 POST requests

--- fefu/fefuapp/urls.py
+++ fefu/fefuapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import text_Post
 
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('about/', views.about_view, name='about'),
+    path('post/', text_Post, name='text_post'),
 ]
--- fefu/fefuapp/views.py
+++ fefu/fefuapp/views.py
@@ -1,7 +1,22 @@
 from django.http import HttpResponse
+from .models import PostData
+from django.views.decorators.csrf import csrf_exempt
 
 def home_view(request):
     return HttpResponse("Hello, World!")
 
 def about_view(request):
     return HttpResponse("О нас")
+
+@csrf_exempt
+def text_Post(request):
+    if request.method == "POST":
+        text = request.POST.get("text", "")
+        if text:
+            PostData.objects.create(text=text)
+            return HttpResponse(f"<h1>Результат</h1><p>{text}</p>")
+        return HttpResponse("<h1>Ошибка</h1><p>Поле 'text' не может быть пустым</p>")
+    return HttpResponse("<h1>Ошибка</h1><p>Метод не поддерживается</p>")
+
+
+
